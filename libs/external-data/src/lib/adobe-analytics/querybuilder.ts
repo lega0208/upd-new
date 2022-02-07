f1bbb8087927f4d3749f9a867747e626597b4e24
shoemakersteve <marclegault@live.com>
2022-02-07 18:06:05 -0500

    Updated CRA segment to new version

--- libs/external-data/src/lib/adobe-analytics/querybuilder.ts
+++ libs/external-data/src/lib/adobe-analytics/querybuilder.ts
@@ -7,7 +7,8 @@ import { ReportQueryDimension } from './aa-dimensions';
 import { metricIds } from './aa-metrics';
 
 export const SEGMENTS = {
-  cra: 's300000938_60e59f8fc002e15213e97a00',
+  cra_old: 's300000938_60e59f8fc002e15213e97a00',
+  cra: 's300000938_61e820b0baaef34505394e51',
   english: 's300000938_57924078e4b05f8496f06d63',
   french: 's300000938_579240a6e4b00bd9617283bd',
   FWYLF: {
