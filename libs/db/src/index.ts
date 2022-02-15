52a61129714a8ce7661daedaa0ed3e050254360c
shoemakersteve <marclegault@live.com>
2022-02-15 10:31:17 -0500

    Added export to schema types

--- libs/db/src/index.ts
+++ libs/db/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/db.module';
 export * from './lib/db.connection';
 export * from './lib/db.schemas';
+export * from './lib/schemas/types'
 
 import mongoose, { Model } from 'mongoose';
 import {
