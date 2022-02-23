4a96015883de97eb331e56f1ea72cd349cd1579f
shoemakersteve <marclegault@live.com>
2022-02-23 12:09:25 -0500

    Export Airtable client

--- libs/external-data/src/index.ts
+++ libs/external-data/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/adobe-analytics';
 export * from './lib/google-search-console';
+export * from './lib/airtable';
 export * from './lib/utils';
 export { DateRange } from './lib/types';
