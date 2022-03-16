25a78ccca470a4f210702062d4b9377277738844
amonsour <pureleb@gmail.com>
2022-03-15 20:55:56 -0400

    Added the rest of Overview
    Fixed the Call Drivers chart

--- libs/upd/components/src/lib/data-card/data-card.component.ts
+++ libs/upd/components/src/lib/data-card/data-card.component.ts
@@ -3,32 +3,32 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-card',
   templateUrl: './data-card.component.html',
-  styleUrls: ['./data-card.component.scss']
+  styleUrls: ['./data-card.component.scss'],
 })
 export class DataCardComponent implements OnInit {
-
   @Input() current = 0;
   @Input() past = 0;
   @Input() title = '';
   arrow: string = '';
   textStyle: string = '';
   comparison = 0;
+  isPast = 0;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.past > 0) {
+      const difference = diff(this.current, this.past);
+      const sign = getSign(difference);
 
-    const difference = diff(this.current, this.past);
-    const sign = getSign(difference);
+      (this.arrow = sign[0]),
+        (this.textStyle = sign[1]),
+        (this.comparison = absoluteNum(difference));
 
-    this.arrow = sign[0],
-    this.textStyle = sign[1],
-    this.comparison = absoluteNum(difference);
-
+      this.isPast = 1;
+    }
   }
-
 }
-
 
 const diff = (a: number, b: number) => {
   return (a - b) / b;
--- libs/upd/views/overview/src/lib/overview-ux-tests/overview-ux-tests.component.ts
+++ libs/upd/views/overview/src/lib/overview-ux-tests/overview-ux-tests.component.ts
@@ -6,7 +6,293 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview-ux-tests.component.css'],
 })
 export class OverviewUxTestsComponent implements OnInit {
+  uxChartData: any[] = [];
+  uxChartCols: any[] = [];
+
+  testsCompleted = 0;
+  tasksTested = 0;
+  participantsTested = 0;
+
+  testsConducted = 0;
+  testsConductedLastQuarter = 0;
+  COPSTests = 0;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.testsCompleted = 36;
+    this.tasksTested = 254;
+    this.participantsTested = 1710;
+    this.testsConducted = 28;
+    this.testsConductedLastQuarter = 4;
+    this.COPSTests = 21;
+
+    this.uxChartData = uxChart;
+
+    this.uxChartCols = [
+      { field: 'UX projects', header: 'UX projects' },
+      { field: 'Test', header: 'Test' },
+      { field: 'Date', header: 'Date' },
+      { field: 'Score', header: 'Score' },
+      { field: 'Participants', header: 'Participants' },
+    ];
+  }
 }
+
+const uxChart = [
+  {
+    'UX projects': 'Tourism and Hospitality Recovery Program (THRP)',
+    Test: 'Baseline',
+    Date: '',
+    Score: 'NAN%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'Disability Tax Credit',
+    Test: 'Validation',
+    Date: '',
+    Score: 'NAN%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'SR&ED',
+    Test: 'Baseline',
+    Date: '',
+    Score: 'NAN%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'Hardest Hit Business Recovery Program (HHBRP)',
+    Test: 'Baseline',
+    Date: '',
+    Score: 'NAN%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'Canada Workers Lockdown Benefit (CWLB)',
+    Test: 'N/A',
+    Date: '',
+    Score: 'NAN%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'End of Life',
+    Test: 'Validation',
+    Date: '',
+    Score: '28%',
+    Participants: 118,
+  },
+  {
+    'UX projects': 'SPR Baseline 5',
+    Test: 'Baseline',
+    Date: '12/2021',
+    Score: '44%',
+    Participants: 48,
+  },
+  {
+    'UX projects': 'SPR Baseline 4',
+    Test: 'Baseline',
+    Date: '11/2021',
+    Score: '48%',
+    Participants: 48,
+  },
+  {
+    'UX projects': 'SPR Baseline 3',
+    Test: 'Baseline',
+    Date: '10/2021',
+    Score: '72%',
+    Participants: 56,
+  },
+  {
+    'UX projects': 'SPR Baseline 2',
+    Test: 'Baseline',
+    Date: '09/2021',
+    Score: '84%',
+    Participants: 56,
+  },
+  {
+    'UX projects': 'SPR Baseline 1',
+    Test: 'Baseline',
+    Date: '09/2021',
+    Score: '96%',
+    Participants: 48,
+  },
+  {
+    'UX projects': 'Canada Recovery Hiring Program (CRHP)',
+    Test: 'Baseline',
+    Date: '09/2021',
+    Score: '66%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'Outreach',
+    Test: 'Validation',
+    Date: '06/2021',
+    Score: '77%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'Cancel or waive penalties & interest',
+    Test: 'Baseline',
+    Date: '06/2021',
+    Score: '63%',
+    Participants: 60,
+  },
+  {
+    'UX projects': 'Task Performance Indicators - (May 2021)',
+    Test: 'Baseline',
+    Date: '05/2021',
+    Score: '79%',
+    Participants: 119,
+  },
+  {
+    'UX projects': 'Payments for Individuals (May 2021)',
+    Test: 'Validation',
+    Date: '05/2021',
+    Score: '61%',
+    Participants: 108,
+  },
+  {
+    'UX projects': 'Payments for Individuals (March 2021)',
+    Test: 'Baseline',
+    Date: '03/2021',
+    Score: '30%',
+    Participants: 108,
+  },
+  {
+    'UX projects': 'How to File 2021',
+    Test: 'Validation',
+    Date: '03/2021',
+    Score: '65%',
+    Participants: 198,
+  },
+  {
+    'UX projects': 'Indigenous Taxes',
+    Test: 'Baseline',
+    Date: '02/2021',
+    Score: '80%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'Scams and fraud',
+    Test: 'Baseline',
+    Date: '01/2021',
+    Score: '68%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'Task Performance Indicators - Baseline (Dec 2020)',
+    Test: 'Baseline',
+    Date: '12/2020',
+    Score: '71%',
+    Participants: 118,
+  },
+  {
+    'UX projects':
+      'Home work space expenses - User experience web content improvements - R3',
+    Test: 'Baseline',
+    Date: '11/2020',
+    Score: '48%',
+    Participants: 48,
+  },
+  {
+    'UX projects': 'CERS pre launch MVP',
+    Test: 'Validation',
+    Date: '11/2020',
+    Score: '63%',
+    Participants: 45,
+  },
+  {
+    'UX projects':
+      'Home work space expenses - User experience web content improvements - baseline (Test 2)',
+    Test: 'Baseline',
+    Date: '10/2020',
+    Score: '42%',
+    Participants: 48,
+  },
+  {
+    'UX projects': 'CRB - Post-launch test',
+    Test: 'Baseline',
+    Date: '10/2020',
+    Score: '55%',
+    Participants: 40,
+  },
+  {
+    'UX projects': 'CRSB  - Post-launch test 1',
+    Test: 'Baseline',
+    Date: '10/2020',
+    Score: '71%',
+    Participants: 48,
+  },
+  {
+    'UX projects': 'CRCB  - Post-launch test 1',
+    Test: 'Baseline',
+    Date: '10/2020',
+    Score: '71%',
+    Participants: 48,
+  },
+  {
+    'UX projects':
+      'Home work space expenses - User experience web content improvements - R1 (prototype)',
+    Test: 'Baseline',
+    Date: '10/2020',
+    Score: '42%',
+    Participants: 48,
+  },
+  {
+    'UX projects': 'Benefits and Credits - UX and web content improvements',
+    Test: 'Baseline',
+    Date: '07/2020',
+    Score: '83%',
+    Participants: 40,
+  },
+  {
+    'UX projects': 'CEWS web pages test',
+    Test: 'Baseline',
+    Date: '07/2020',
+    Score: '55%',
+    Participants: 40,
+  },
+  {
+    'UX projects': 'CEWS Spreadsheet',
+    Test: 'Baseline',
+    Date: '06/2020',
+    Score: '15%',
+    Participants: 20,
+  },
+  {
+    'UX projects': 'CERB 2.0 Repayment  - exploratory (after widget launch)',
+    Test: 'Exploratory',
+    Date: '05/2020',
+    Score: '43%',
+    Participants: 40,
+  },
+  {
+    'UX projects': 'CERB 2.0 Repayment  - baseline (before widget launch)',
+    Test: 'Baseline',
+    Date: '05/2020',
+    Score: '45%',
+    Participants: 40,
+  },
+  {
+    'UX projects': 'Contact the CRA',
+    Test: 'Validation',
+    Date: '01/2019',
+    Score: '62%',
+    Participants: 120,
+  },
+  {
+    'UX projects': 'Canada Child Benefit (CCB)',
+    Test: 'Validation',
+    Date: '01/2019',
+    Score: '78%',
+    Participants: 0,
+  },
+  {
+    'UX projects': 'GST/HST and Payroll',
+    Test: 'Validation',
+    Date: '01/2018',
+    Score: '62%',
+    Participants: 0,
+  },
+];
