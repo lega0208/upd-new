4e336b3a3fd71be7c2547fe8fa86607a45ab1527
shoemakersteve <marclegault@live.com>
2022-03-23 13:59:15 -0400

    Added Effect to update data on date selection change

--- libs/upd/views/pages/src/lib/pages-details/+state/pages-details.effects.ts
+++ libs/upd/views/pages/src/lib/pages-details/+state/pages-details.effects.ts
@@ -4,7 +4,7 @@ import { Store } from '@ngrx/store';
 import { catchError, EMPTY, mergeMap, map, of } from 'rxjs';
 import { ApiService } from '@cra-arc/upd/services';
 
-import { selectDateRanges, selectRouteNestedParam } from '@cra-arc/upd/state';
+import { selectDateRanges, selectRouteNestedParam, selectDatePeriod } from '@cra-arc/upd/state';
 import * as PagesDetailsActions from './pages-details.actions';
 import * as PagesDetailsSelectors from './pages-details.selectors';
 
@@ -18,15 +18,15 @@ export class PagesDetailsEffects {
         this.store$.select(selectDateRanges),
         this.store$.select(PagesDetailsSelectors.selectPagesDetailsData),
       ]),
-      mergeMap(([, pageId, { dateRange, comparisonDateRange }, pagesData]) => {
+      mergeMap(([, pageId, { dateRange, comparisonDateRange }, pageDetailsData]) => {
         if (!pageId) {
           console.error('pageId not found when trying to load page details');
         }
 
-        const pageIsLoaded = pagesData._id === pageId; // page is already loaded (but not necessarily with the correct data)
-        const dateRangeIsLoaded = pagesData.dateRange === dateRange; // data for the dateRange is already loaded
+        const pageIsLoaded = pageDetailsData._id === pageId; // page is already loaded (but not necessarily with the correct data)
+        const dateRangeIsLoaded = pageDetailsData.dateRange === dateRange; // data for the dateRange is already loaded
         const comparisonDateRangeIsLoaded =
-          pagesData.comparisonDateRange === comparisonDateRange;
+          pageDetailsData.comparisonDateRange === comparisonDateRange;
 
         if (pageIsLoaded && dateRangeIsLoaded && comparisonDateRangeIsLoaded) {
           // if everything is already loaded in the state, don't update it
@@ -44,6 +44,13 @@ export class PagesDetailsEffects {
             catchError(() => EMPTY)
           );
       })
+    )
+  );
+
+  dateChange$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(selectDatePeriod),
+      mergeMap(() => of(PagesDetailsActions.loadPagesDetailsInit()))
     )
   );
 
