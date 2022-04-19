f0bd19db0bfa38a52bfa3b3caa9e0508c9044b32
kolekrstev <65304466+kolekrstev@users.noreply.github.com>
2022-04-19 01:14:14 -0700

    Translation updates - part 2

--- libs/upd/views/overview/src/lib/overview-webtraffic/overview-webtraffic.component.ts
+++ libs/upd/views/overview/src/lib/overview-webtraffic/overview-webtraffic.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ColumnConfig } from '@cra-arc/upd-components';
 import { OverviewFacade } from '../+state/overview/overview.facade';
+import { I18nFacade } from '@cra-arc/upd/state';
+import { EN_CA, LocaleId } from '@cra-arc/upd/i18n';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-overview-webtraffic',
@@ -8,6 +11,9 @@ import { OverviewFacade } from '../+state/overview/overview.facade';
   styleUrls: ['./overview-webtraffic.component.css'],
 })
 export class OverviewWebtrafficComponent {
+  currentLang!: LocaleId;
+  currentLang$ = this.i18n.currentLang$;
+
   uniqueVisitors$ = this.overviewService.visitors$;
   uniqueVisitorsPercentChange$ = this.overviewService.visitorsPercentChange$;
 
@@ -30,6 +36,7 @@ export class OverviewWebtrafficComponent {
   label = 'Visits';
 
   dateRangeLabel$ = this.overviewService.dateRangeLabel$;
+  comparisonDateRangeLabel$ = this.overviewService.comparisonDateRangeLabel$;
 
   barTableCols: ColumnConfig[] = [
     { field: 'name', header: 'Dates' },
@@ -41,5 +48,33 @@ export class OverviewWebtrafficComponent {
     { field: 'prevValue', header: 'Visits for ', pipe: 'number' },
   ];
 
-  constructor(private overviewService: OverviewFacade) {}
+  constructor(
+    private overviewService: OverviewFacade,  
+    private i18n: I18nFacade
+  ) {}
+
+  dyfTableCols2: ColumnConfig[] = [];
+  topPagesCols2: ColumnConfig[] = [];
+  barTableCols2: ColumnConfig[] = [];
+
+  ngOnInit() {
+    this.i18n.service.onLangChange(
+      ({ lang }) => { this.currentLang = lang as LocaleId; }
+    );
+
+    combineLatest([this.currentLang$, this.dateRangeLabel$, this.comparisonDateRangeLabel$]).subscribe(([lang, dateRange, comparisonDateRange]) => {
+      
+      this.topPagesCols2 = [
+        { field: '_id', header: this.i18n.service.translate('URL', lang) },
+        { field: 'visits', header: this.i18n.service.translate('visits', lang), pipe: 'number' },
+        { field: 'comparison',  header: this.i18n.service.translate('comparison', lang)},
+      ];
+      this.barTableCols2 = [
+        { field: 'name', header: this.i18n.service.translate('Dates', lang) },
+        //{ field: 'currValue', header: `Visits for ${this.dateRangeLabel$}`, pipe: 'number' },
+        { field: 'currValue', header: this.i18n.service.translate('Visits for ', lang, {value: dateRange  }), pipe: 'number' },
+        { field: 'prevValue', header: this.i18n.service.translate('Visits for ', lang, {value: comparisonDateRange  }), pipe: 'number' }
+      ];
+    });
+  }
 }
