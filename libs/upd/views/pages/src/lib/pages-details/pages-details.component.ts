0e44fa69cc714c2367cf0ff3849e720db7ff442c
shoemakersteve <marclegault@live.com>
2022-04-25 14:33:08 -0400

    Add lang path to back button link

--- libs/upd/views/pages/src/lib/pages-details/pages-details.component.ts
+++ libs/upd/views/pages/src/lib/pages-details/pages-details.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PagesDetailsFacade } from './+state/pages-details.facade';
 
-//import { TranslateService } from '@ngx-translate/core';
-
 import { ColumnConfig } from '@cra-arc/upd-components';
 import { I18nFacade } from '@cra-arc/upd/state';
 import { combineLatest } from 'rxjs';
+import { EN_CA } from '@cra-arc/upd/i18n';
 
 @Component({
   selector: 'app-page-details',
@@ -16,33 +15,22 @@ export class PagesDetailsComponent implements OnInit {
   title$ = this.pageDetailsService.pageTitle$;
   url$ = this.pageDetailsService.pageUrl$;
   loading$ = this.pageDetailsService.loading$;
-  startSession$ = this.pageDetailsService.startSession$;
   currentLang$ = this.i18n.currentLang$;
   showUrl = true;
   showAlert = false;
 
-  // navTabs: { href: string; title: string }[] = [
-  //   { href: 'summary', title: 'Summary' },
-  //   { href: 'webtraffic', title: 'Web Traffic' },
-  //   { href: 'searchanalytics', title: 'Search Analytics' },
-  //   { href: 'pagefeedback', title: 'Page Feedback' },
-  //   // { href: 'details', title: 'Details' },
-  // ];
-
+  langLink = 'en';
   navTabs: { href: string; title: string }[] = []
 
   constructor(
     private pageDetailsService: PagesDetailsFacade,
-    //public translateService: TranslateService,
     private i18n: I18nFacade
   ) {}
 
   ngOnInit(): void {
     this.pageDetailsService.init();
-    +
-    combineLatest([
-      this.currentLang$
-    ]).subscribe(([lang]) => {
+
+    this.currentLang$.subscribe((lang) => {
       this.navTabs = [
         { href: 'summary', title: this.i18n.service.translate('tab-summary', lang) },
         { href: 'webtraffic', title: this.i18n.service.translate('tab-webtraffic', lang) },
@@ -50,6 +38,8 @@ export class PagesDetailsComponent implements OnInit {
         { href: 'pagefeedback', title: this.i18n.service.translate('tab-pagefeedback', lang) },
         // { href: 'details', title: this.i18n.service.translate('tab-details', lang) },
       ];
+
+      this.langLink = lang === EN_CA ? 'en' : 'fr';
     });
   }
 
