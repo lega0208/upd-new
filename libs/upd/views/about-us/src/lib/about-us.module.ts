57ab899dd47adc31e5c37349932629895fb47978
achouk <achoukings@gmail.com>
2022-07-05 16:15:55 -0400

    Added About Us page

--- libs/upd/views/about-us/src/lib/about-us.module.ts
+++ libs/upd/views/about-us/src/lib/about-us.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us.component';
+import { I18nModule } from '@dua-upd/upd/i18n';
+import { I18nFacade } from '@dua-upd/upd/state';
 
 @NgModule({
   imports: [
+    I18nModule,
     CommonModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: AboutUsComponent }
