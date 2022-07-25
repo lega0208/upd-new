92a3feacbe2e91dfa15e5ff7ac0b67cc651826b7
Anthony <anthony.debassige@rogers.com>
2022-07-25 13:37:48 -0400

    Update test spec for overview-feedback

--- libs/upd/views/overview/src/lib/overview-feedback/overview-feedback.component.spec.ts
+++ libs/upd/views/overview/src/lib/overview-feedback/overview-feedback.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { StoreModule } from '@ngrx/store';
+import { routerReducer } from '@ngrx/router-store';
+import {
+  dateSelectionReducer,
+  I18nFacade,
+  i18nReducer,
+} from '@dua-upd/upd/state';
+import { I18nModule } from '@dua-upd/upd/i18n';
 import { OverviewFeedbackComponent } from './overview-feedback.component';
+import { OverviewFacade } from '../+state/overview/overview.facade';
+import {
+  OVERVIEW_FEATURE_KEY,
+  overviewReducer,
+} from '../+state/overview/overview.reducer';
+import { OverviewRoutingModule } from '../overview-routing.module';
+import { UpdComponentsModule } from '@dua-upd/upd-components';
+
+import '@angular/localize/init';
 
 describe('OverviewFeedbackComponent', () => {
   let component: OverviewFeedbackComponent;
@@ -8,7 +25,19 @@ describe('OverviewFeedbackComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        I18nModule.forRoot(),
+        StoreModule.forRoot({
+          dateSelection: dateSelectionReducer,
+          router: routerReducer,
+          i18n: i18nReducer,
+        }),
+        StoreModule.forFeature(OVERVIEW_FEATURE_KEY, overviewReducer),
+        OverviewRoutingModule,
+        UpdComponentsModule,
+      ],
       declarations: [OverviewFeedbackComponent],
+      providers: [I18nFacade, OverviewFacade],
     }).compileComponents();
   });
 
