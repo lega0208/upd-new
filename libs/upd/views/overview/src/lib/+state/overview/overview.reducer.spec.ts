62aaca98d498d67053cec176efa751c50dfc31a9
Anthony <anthony.debassige@rogers.com>
2022-07-28 10:12:44 -0400

    test overview.reducer no operations action returns the initial state
    upd-views-overview project update for overview reducer: NOOP default action should return the initial state

--- libs/upd/views/overview/src/lib/+state/overview/overview.reducer.spec.ts
+++ libs/upd/views/overview/src/lib/+state/overview/overview.reducer.spec.ts
@@ -1,10 +1,40 @@
-import { Action } from '@ngrx/store';
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { NxModule } from '@nrwl/angular';
+
+import { createReducer, on, Action } from '@ngrx/store';
 
 import * as OverviewActions from './overview.actions';
-import { OverviewData } from './overview.models';
-import { OverviewState, initialState, reducer } from './overview.reducer';
+import { OverviewData } from '@dua-upd/types-common';
+import { OverviewState, initialState, overviewReducer } from './overview.reducer';
+
+import { Observable } from 'rxjs';
 
 describe('Overview Reducer', () => {
+  let actions: Observable<Action>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NxModule.forRoot()],
+      providers: [
+        overviewReducer,
+        provideMockActions(() => actions),
+        provideMockStore(),
+      ],
+    });
+
+//    effects = TestBed.inject(OverviewEffects);
+  });
+
+  it('should return the default state', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const action = {type: 'NOOP'} as any;
+    const result = overviewReducer(undefined, action);
+    expect(result).toBe(initialState);
+  });
+
+  /**
   const createOverviewEntity = (id: string, name = ''): OverviewData => ({
     id,
     name: name || `name-${id}`,
@@ -18,10 +48,12 @@ describe('Overview Reducer', () => {
       ];
       const action = OverviewActions.loadOverviewSuccess({ overview });
 
-      const result: OverviewState = reducer(initialState, action);
+
+
+      const result: OverviewState = overviewReducer(initialState, action);
 
       expect(result.loaded).toBe(true);
-      expect(result.ids.length).toBe(2);
+      expect(result.data.ids.length).toBe(2);
     });
   });
 
@@ -29,9 +61,12 @@ describe('Overview Reducer', () => {
     it('should return the previous state', () => {
       const action = {} as Action;
 
-      const result = reducer(initialState, action);
+      const result = overviewReducer(initialState, action);
 
       expect(result).toBe(initialState);
     });
   });
+
+*/
+
 });
