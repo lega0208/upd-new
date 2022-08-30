2f9f41dcb0521c0c3f60d2e6fb58b0a373edc78b
lega0208 <marclegault@Live.com>
2022-08-30 12:16:12 -0400

    Fix circular dependency in State sanitizer

--- libs/upd/state/src/lib/state.sanitizers.ts
+++ libs/upd/state/src/lib/state.sanitizers.ts
@@ -1,6 +1,5 @@
 import { Action } from '@ngrx/store';
 import { PagesHomeData } from '@dua-upd/types-common';
-import { PagesHomeState } from '@dua-upd/upd/views/pages';
 
 interface PagesHomeAction extends Action {
   data: PagesHomeData;
@@ -12,13 +11,13 @@ export const actionSanitizer = (action: Action) =>
     ? { ...action, data: '<<LONG_BLOB>>' }
     : action;
 
-export const stateSanitizer = (state: { pagesHome: PagesHomeState }) =>
-  state.pagesHome?.data
+export const stateSanitizer = (state: { pagesHome: { data: unknown } }) =>
+  state?.pagesHome?.data
     ? {
-      ...state,
-      pagesHome: {
-        ...state.pagesHome,
-        data: '<<PAGES-HOME_DATA>>',
-      },
-    }
-    : state
+        ...state,
+        pagesHome: {
+          ...state.pagesHome,
+          data: '<<PAGES-HOME_DATA>>',
+        },
+      }
+    : state;
