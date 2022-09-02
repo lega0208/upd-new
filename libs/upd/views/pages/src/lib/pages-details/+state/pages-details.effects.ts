c21ba9436fb75bdc31687aebcf3710fc0971d4b7
lega0208 <marclegault@Live.com>
2022-09-02 15:43:53 -0400

    Minor formatting & naming tweaks (Pages home/details)"

--- libs/upd/views/pages/src/lib/pages-details/+state/pages-details.effects.ts
+++ libs/upd/views/pages/src/lib/pages-details/+state/pages-details.effects.ts
@@ -4,59 +4,93 @@ import { Store } from '@ngrx/store';
 import { catchError, EMPTY, mergeMap, map, of } from 'rxjs';
 import { ApiService } from '@dua-upd/upd/services';
 
-import { selectDateRanges, selectRouteNestedParam, selectDatePeriod } from '@dua-upd/upd/state';
+import {
+  selectDateRanges,
+  selectRouteNestedParam,
+  selectDatePeriod,
+} from '@dua-upd/upd/state';
 import * as PagesDetailsActions from './pages-details.actions';
 import * as PagesDetailsSelectors from './pages-details.selectors';
 
 @Injectable()
 export class PagesDetailsEffects {
-  init$ = createEffect(() =>
-    this.actions$.pipe(
+  init$ = createEffect(() => {
+    return this.actions$.pipe(
       ofType(PagesDetailsActions.loadPagesDetailsInit),
       concatLatestFrom(() => [
-        this.store$.select(selectRouteNestedParam('id')),
-        this.store$.select(selectDateRanges),
-        this.store$.select(PagesDetailsSelectors.selectPagesDetailsData),
+        this.store.select(selectRouteNestedParam('id')),
+        this.store.select(selectDateRanges),
+        this.store.select(PagesDetailsSelectors.selectPagesDetailsData),
       ]),
-      mergeMap(([, pageId, { dateRange, comparisonDateRange }, pageDetailsData]) => {
-        if (!pageId) {
-          console.error('pageId not found when trying to load page details');
+      mergeMap(
+        ([, pageId, { dateRange, comparisonDateRange }, pageDetailsData]) => {
+          if (!pageId) {
+            console.error('pageId not found when trying to load page details');
+          }
+
+          const pageIsLoaded = pageDetailsData._id === pageId; // page is already loaded (but not necessarily with the correct data)
+          const dateRangeIsLoaded = pageDetailsData.dateRange === dateRange; // data for the dateRange is already loaded
+          const comparisonDateRangeIsLoaded =
+            pageDetailsData.comparisonDateRange === comparisonDateRange;
+
+          if (
+            pageIsLoaded &&
+            dateRangeIsLoaded &&
+            comparisonDateRangeIsLoaded
+          ) {
+            // if everything is already loaded in the state, don't update it
+            return of(
+              PagesDetailsActions.loadPagesDetailsSuccess({ data: null })
+            );
+          }
+
+          return this.api
+            .getPageDetails({
+              id: pageId,
+              dateRange,
+              ...{ comparisonDateRange },
+            })
+            .pipe(
+              map((data) =>
+                PagesDetailsActions.loadPagesDetailsSuccess({ data })
+              ),
+              catchError(() => EMPTY)
+            );
         }
+      )
+    );
+  });
 
-        const pageIsLoaded = pageDetailsData._id === pageId; // page is already loaded (but not necessarily with the correct data)
-        const dateRangeIsLoaded = pageDetailsData.dateRange === dateRange; // data for the dateRange is already loaded
-        const comparisonDateRangeIsLoaded =
-          pageDetailsData.comparisonDateRange === comparisonDateRange;
+  pageIsLoaded = false;
 
-        if (pageIsLoaded && dateRangeIsLoaded && comparisonDateRangeIsLoaded) {
-          // if everything is already loaded in the state, don't update it
-          return of(
-            PagesDetailsActions.loadPagesDetailsSuccess({ data: null })
-          );
-        }
+  dateChange$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(selectDatePeriod),
+        concatLatestFrom(() => [
+          this.store.select(selectRouteNestedParam('id')),
+        ]),
+        mergeMap(([, id]) => {
+          if (!id) {
+            this.pageIsLoaded = false;
 
-        return this.api
-          .getPageDetails({ id: pageId, dateRange, ...{ comparisonDateRange } })
-          .pipe(
-            map((data) =>
-              PagesDetailsActions.loadPagesDetailsSuccess({ data })
-            ),
-            catchError(() => EMPTY)
-          );
-      })
-    )
-  );
+            return of(EMPTY);
+          }
 
-  dateChange$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(selectDatePeriod),
-      mergeMap(() => of(PagesDetailsActions.loadPagesDetailsInit()))
-    )
+          this.pageIsLoaded = true;
+
+          return of(PagesDetailsActions.loadPagesDetailsInit());
+        })
+      );
+    },
+    {
+      dispatch: this.pageIsLoaded,
+    }
   );
 
   constructor(
     private readonly actions$: Actions,
     private api: ApiService,
-    private store$: Store
+    private store: Store
   ) {}
 }
--- libs/upd/views/pages/src/lib/pages-home/+state/pages-home.selectors.ts
+++ libs/upd/views/pages/src/lib/pages-home/+state/pages-home.selectors.ts
@@ -2,27 +2,27 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { PAGES_HOME_FEATURE_KEY, PagesHomeState } from './pages-home.reducer';
 
 // Lookup the 'PagesHome' feature state managed by NgRx
-export const getPagesHomeState = createFeatureSelector<PagesHomeState>(
+export const selectPagesHomeState = createFeatureSelector<PagesHomeState>(
   PAGES_HOME_FEATURE_KEY
 );
 
-export const getPagesHomeLoaded = createSelector(
-  getPagesHomeState,
+export const selectPagesHomeLoaded = createSelector(
+  selectPagesHomeState,
   (state: PagesHomeState) => state.loaded
 );
 
-export const getPagesHomeLoading = createSelector(
-  getPagesHomeState,
+export const selectPagesHomeLoading = createSelector(
+  selectPagesHomeState,
   (state: PagesHomeState) => state.loading
 );
 
-export const getPagesHomeError = createSelector(
-  getPagesHomeState,
+export const selectPagesHomeError = createSelector(
+  selectPagesHomeState,
   (state: PagesHomeState) => state.error
 );
 
-export const getPagesHomeData = createSelector(
-  getPagesHomeState,
+export const selectPagesHomeData = createSelector(
+  selectPagesHomeState,
   (state: PagesHomeState) => ({
     ...(state.data || {}),
   })
