e70b86c71092a0cf0c1cd44278d34f9a681d29ec
lega0208 <marclegault@Live.com>
2022-11-22 20:15:17 -0500

    Forgot to remove references to deleted Service

--- libs/external-data/src/lib/external-data.module.ts
+++ libs/external-data/src/lib/external-data.module.ts
@@ -1,19 +1,16 @@
 import { Module, ConsoleLogger } from '@nestjs/common';
 import { AdobeAnalyticsService } from './adobe-analytics/adobe-analytics.service';
 import { GoogleSearchConsoleService } from './google-search-console/google-search-console.service';
-import { AirtableService } from './airtable/airtable.service';
 
 @Module({
   providers: [
     AdobeAnalyticsService,
     GoogleSearchConsoleService,
-    AirtableService,
     ConsoleLogger,
   ],
   exports: [
     AdobeAnalyticsService,
     GoogleSearchConsoleService,
-    AirtableService,
   ],
 })
 export class ExternalDataModule {}
