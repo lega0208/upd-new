0c1ffd5cd35962949f6845a52ffbe44b7225a77d
lega0208 <marclegault@Live.com>
2022-12-21 12:18:13 -0500

    Script for fixing airtable page issues

--- apps/db-cli/src/app/scripts/scratchpad.ts
+++ apps/db-cli/src/app/scripts/scratchpad.ts
@@ -1,4 +1,7 @@
 import { DbService } from '@dua-upd/db';
+import { DbUpdateService } from '@dua-upd/db-update';
+import { Types } from 'mongoose';
+import { logJson } from '@dua-upd/utils-common';
 
 /*
  *  Export any function and it'll show up with the "run-script" command.
@@ -12,3 +15,72 @@ import { DbService } from '@dua-upd/db';
 export const findDuplicatePageUrls = async (db: DbService) => {
   await db.getDuplicatedPages();
 };
+
+export const fixPagesFromAirtable = async (db: DbService, dbUpdate: DbUpdateService) => {
+  console.time('fixPagesFromAirtable');
+  // delete pages that need to be repopulated
+  const objectIdsToDelete = [
+    new Types.ObjectId('63867f9f2b89cb3c58edafdf'),
+    new Types.ObjectId('63867f9f2b89cb3c58edafe0'),
+  ]
+  await db.collections.pages.deleteMany({ _id: { $in: objectIdsToDelete } });
+
+  // update airtable data
+  await dbUpdate.updateUxData();
+
+  // consolidate duplicate pages
+  await dbUpdate.consolidateDuplicatePages();
+
+  // run validatePageRefs with filter for old ids
+  await db.validatePageMetricsRefs({ page: { $in: objectIdsToDelete } });
+
+  // clear pageVisits view and repopulate
+  await db.views.pageVisits.clearAll()
+
+  const dateRangesToRepopulate = [
+    // yearly
+    {
+      start: new Date('2021-01-01'),
+      end: new Date('2021-12-31'),
+    },
+    {
+      start: new Date('2020-01-03'),
+      end: new Date('2021-01-01'),
+    },
+    // quarterly
+    {
+      start: new Date('2022-07-01'),
+      end: new Date('2022-09-30'),
+    },
+    {
+      start: new Date('2022-04-01'),
+      end: new Date('2022-07-01'),
+    },
+    // monthly
+    {
+      start: new Date('2022-11-01'),
+      end: new Date('2022-11-30'),
+    },
+    {
+      start: new Date('2022-09-27'),
+      end: new Date('2022-10-26'),
+    },
+    // weekly
+    {
+      start: new Date('2022-12-11'),
+      end: new Date('2022-12-17'),
+    },
+    {
+      start: new Date('2022-12-04'),
+      end: new Date('2022-12-10'),
+    },
+  ];
+
+  for (const dateRange of dateRangesToRepopulate) {
+    console.log('Repopulating date range:');
+    logJson(dateRange);
+    await db.views.pageVisits.getOrUpdate(dateRange);
+  }
+
+  console.timeEnd('fixPagesFromAirtable');
+}
