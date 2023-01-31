0d0f4d96c19f38ce1dc52414b9affa3b1c48c46a
lega0208 <marclegault@Live.com>
2023-01-31 16:28:23 -0500

    Add a few missing translations; Made overview tables sortable; Simplified & restyled Task details;

--- libs/upd/views/overview/src/lib/overview-search-analytics/overview-search-analytics.component.ts
+++ libs/upd/views/overview/src/lib/overview-search-analytics/overview-search-analytics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { combineLatest } from 'rxjs';
+import { combineLatest, map } from 'rxjs';
 import { ColumnConfig } from '@dua-upd/upd-components';
 import { I18nFacade } from '@dua-upd/upd/state';
 import { OverviewFacade } from '../+state/overview/overview.facade';
@@ -39,8 +39,12 @@ export class OverviewSearchAnalyticsComponent implements OnInit {
   satStart = '';
   satEnd = '';
 
-  topSearchTermsEn$ = this.overviewService.top20SearchTermsEn$;
-  topSearchTermsFr$ = this.overviewService.top20SearchTermsFr$;
+  topSearchTermsEn$ = this.overviewService.top20SearchTermsEn$.pipe(
+    map((searchTerms) => [...searchTerms])
+  );
+  topSearchTermsFr$ = this.overviewService.top20SearchTermsFr$.pipe(
+    map((searchTerms) => [...searchTerms])
+  );
   searchTermsColConfig$ = this.overviewService.searchTermsColConfig$;
 
   constructor(
@@ -49,96 +53,98 @@ export class OverviewSearchAnalyticsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    combineLatest([this.searchAssessmentData$, this.currentLang$, this.satDateRangeLabel$]).subscribe(
-      ([data, lang, satDateRange]) => {
-        this.satStart = satDateRange.split('-')[0];
-        this.satEnd = satDateRange.split('-')[1];
+    combineLatest([
+      this.searchAssessmentData$,
+      this.currentLang$,
+      this.satDateRangeLabel$,
+    ]).subscribe(([data, lang, satDateRange]) => {
+      this.satStart = satDateRange.split('-')[0];
+      this.satEnd = satDateRange.split('-')[1];
 
-        // this.CanSearchTermsCols = [
-        //   { field: 'Search terms', header: this.i18n.service.translate('search-terms', lang) },
-        //   { field: 'Clicks', header: this.i18n.service.translate('clicks', lang) },
-        //   { field: 'Comparison', header: this.i18n.service.translate('comparison', lang) }
-        // ];
-        this.GSCSearchTermsCols = [
-          {
-            field: '_id',
-            header: this.i18n.service.translate('search-term', lang),
-          },
-          {
-            field: 'clicks',
-            header: this.i18n.service.translate('clicks', lang),
-            pipe: 'number',
-          },
-          {
-            field: 'percentChange',
-            header: this.i18n.service.translate('comparison', lang),
-            pipe: 'percent',
-          },
-          {
-            field: 'impressions',
-            header: this.i18n.service.translate('impressions', lang),
-            pipe: 'number',
-          },
-          {
-            field: 'ctr',
-            header: this.i18n.service.translate('ctr', lang),
-            pipe: 'percent',
-          },
-          {
-            field: 'position',
-            header: this.i18n.service.translate('position', lang),
-            pipe: 'number',
-            pipeParam: '1.0-2',
-          },
-        ];
+      // this.CanSearchTermsCols = [
+      //   { field: 'Search terms', header: this.i18n.service.translate('search-terms', lang) },
+      //   { field: 'Clicks', header: this.i18n.service.translate('clicks', lang) },
+      //   { field: 'Comparison', header: this.i18n.service.translate('comparison', lang) }
+      // ];
+      this.GSCSearchTermsCols = [
+        {
+          field: '_id',
+          header: this.i18n.service.translate('search-term', lang),
+        },
+        {
+          field: 'clicks',
+          header: this.i18n.service.translate('clicks', lang),
+          pipe: 'number',
+        },
+        {
+          field: 'percentChange',
+          header: this.i18n.service.translate('comparison', lang),
+          pipe: 'percent',
+        },
+        {
+          field: 'impressions',
+          header: this.i18n.service.translate('impressions', lang),
+          pipe: 'number',
+        },
+        {
+          field: 'ctr',
+          header: this.i18n.service.translate('ctr', lang),
+          pipe: 'percent',
+        },
+        {
+          field: 'position',
+          header: this.i18n.service.translate('position', lang),
+          pipe: 'number',
+          pipeParam: '1.0-2',
+        },
+      ];
 
-        this.searchAssessmentCols = [
-          {
-            field: 'query',
-            header: this.i18n.service.translate('search-term', lang),
+      this.searchAssessmentCols = [
+        {
+          field: 'query',
+          header: this.i18n.service.translate('search-term', lang),
+        },
+        {
+          field: 'lang',
+          header: this.i18n.service.translate('Search term language', lang),
+          filterConfig: {
+            type: 'category',
+            categories: createCategoryConfig({
+              i18n: this.i18n.service,
+              data,
+              field: 'lang',
+            }),
           },
-          {
-            field: 'lang',
-            header: this.i18n.service.translate('Search term language', lang),
-            filterConfig: {
-              type: 'category',
-              categories: createCategoryConfig({
-                i18n: this.i18n.service,
-                data,
-                field: 'lang',
-              }),
-            },
+        },
+        {
+          field: 'url',
+          header: this.i18n.service.translate('Target URL', lang),
+          type: 'link',
+          typeParams: {
+            external: true,
+            link: 'url',
           },
-          {
-            field: 'url',
-            header: this.i18n.service.translate('Target URL', lang),
-            type: 'link',
-            typeParams: {
-              external: true,
-              link: 'url',
-            },
-          },
-          {
-            field: 'total_searches',
-            header: this.i18n.service.translate('Total searches', lang),
-          },
-          {
-            field: 'target_clicks',
-            header: this.i18n.service.translate('Target URL clicks', lang),
-          },
-          {
-            field: 'position',
-            header: this.i18n.service.translate('avg-rank', lang),
-          },
-          {
-            field: 'pass',
-            header: this.i18n.service.translate('Result', lang),
-            type: 'text',
-            typeParam: 'passFail',
-          },
-        ];
-      }
-    );
+        },
+        {
+          field: 'total_searches',
+          header: this.i18n.service.translate('Total searches', lang),
+        },
+        {
+          field: 'target_clicks',
+          header: this.i18n.service.translate('Target URL clicks', lang),
+        },
+        {
+          field: 'position',
+          header: this.i18n.service.translate('avg-rank', lang),
+        },
+        {
+          field: 'pass',
+          header: this.i18n.service.translate('Result', lang),
+          type: 'text',
+          typeParam: 'passFail',
+        },
+      ];
+    });
   }
 }
 
