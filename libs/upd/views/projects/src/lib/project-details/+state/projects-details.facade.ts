49ed9112c25d6724c1f9133fcfa9b98cf69ad378
lega0208 <marclegault@Live.com>
2023-05-26 17:01:37 +0000

    Update libs/upd/views/projects/src/lib/project-details/+state/projects-details.facade.ts

--- libs/upd/views/projects/src/lib/project-details/+state/projects-details.facade.ts
+++ libs/upd/views/projects/src/lib/project-details/+state/projects-details.facade.ts
@@ -484,7 +484,7 @@ export class ProjectsDetailsFacade {
   ]).pipe(
     map(([data, lang]) => {
       const uxTests = data?.taskSuccessByUxTest.filter(
-        (uxTest) => uxTest.success_rate !== undefined
+        (uxTest) => uxTest.success_rate || uxTest.success_rate === 0
       );
 
       const tasksWithSuccessRate = uxTests
