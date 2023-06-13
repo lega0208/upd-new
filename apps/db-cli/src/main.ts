6599f960d2fc9217c1ce86406d2b9f7c892bd925
Marc Legault <marclegault@live.com>
2023-06-13 20:43:53 +0000

    [db-cli] Added inject method for easier access to deps (see example)

--- apps/db-cli/src/main.ts
+++ apps/db-cli/src/main.ts
@@ -1,9 +1,11 @@
+import { exit } from 'node:process';
 import { CommandFactory } from 'nest-commander';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   await CommandFactory.run(AppModule, ['warn', 'error']);
+  exit();
 }
 
 bootstrap().catch((e) => {
