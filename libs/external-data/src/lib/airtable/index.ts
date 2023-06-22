60007de3dceab9ffb17daab00bfd105a803f9ffd
amonsour <pureleb@gmail.com>
2023-06-22 16:04:58 +0000

    Updated function titles

--- libs/external-data/src/lib/airtable/index.ts
+++ libs/external-data/src/lib/airtable/index.ts
@@ -430,11 +430,11 @@ export class AirtableClient {
       })) as FeedbackData[];
   }
 
-  async getMainFeedback(
+  async getLiveFeedback(
     dateRange: { start: DateType; end: DateType } = {} as typeof dateRange
   ) {
     const filterByFormula = createDateRangeFilterFormula(dateRange, 'Date');
-    const query = this.createQuery(bases.MAIN_FEEDBACK, 'Page feedback', {
+    const query = this.createQuery(bases.LIVE_FEEDBACK, 'Page feedback', {
       filterByFormula,
     });
     
