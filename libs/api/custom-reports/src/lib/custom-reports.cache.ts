3a56ca29c553a016fa0185f871c8fea384a121e6
lega0208 <marclegault@Live.com>
2024-01-19 21:03:42 +0000

    Cache reports on initial assembly; Add cache compression

--- libs/api/custom-reports/src/lib/custom-reports.cache.ts
+++ libs/api/custom-reports/src/lib/custom-reports.cache.ts
@@ -1,6 +1,7 @@
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 import type { Cache } from 'cache-manager';
+import { compressString, decompressString } from '@dua-upd/node-utils';
 import type { ReportDataPoint } from './custom-reports.service';
 
 type Report = Record<string, unknown>[];
@@ -10,15 +11,25 @@ export class CustomReportsCache {
   constructor(@Inject(CACHE_MANAGER) private cache: Cache) {}
 
   async getReport(reportId: string): Promise<Report | undefined> {
-    return await this.cache.get<Report>(`report:${reportId}`);
+    return this.cache
+      .get<string>(`report:${reportId}`)
+      .then(
+        async (report) => report && JSON.parse(await decompressString(report)),
+      );
   }
 
   async setReport(reportId: string, report: Report) {
-    return await this.cache.set(`report:${reportId}`, report);
+    return this.cache.store.set(
+      `report:${reportId}`,
+      await compressString(JSON.stringify(report)),
+    );
   }
 
   async getDataPoints(dataPointHashes: string[]) {
-    return await this.cache.store.mget(...dataPointHashes) as Record<string, ReportDataPoint>[];
+    return (await this.cache.store.mget(...dataPointHashes)) as Record<
+      string,
+      ReportDataPoint
+    >[];
   }
 
   async setDataPoints(dataPoints: Record<string, ReportDataPoint>) {
--- libs/api/custom-reports/src/lib/custom-reports.processors.ts
+++ libs/api/custom-reports/src/lib/custom-reports.processors.ts
@@ -4,6 +4,7 @@ import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { AdobeAnalyticsClient } from './clients/adobe-analytics.client';
 import { createQuery } from './clients/adobe-analytics.query';
+import { CustomReportsCache } from './custom-reports.cache';
 import {
   ChildJobMetadata,
   CustomReportsService,
@@ -23,15 +24,24 @@ type ReportCreationMetadata = {
  */
 @Processor('prepareReportData')
 export class PrepareReportDataProcessor extends WorkerHost {
-  constructor(private db: DbService) {
+  constructor(
+    private db: DbService,
+    private cache: CustomReportsCache,
+  ) {
     super();
   }
 
   async process(job: Job<ReportCreationMetadata, void, string>) {
     try {
-      return await this.db.collections.customReportsMetrics.getReport(
+      const report = await this.db.collections.customReportsMetrics.getReport(
         job.data.config,
       );
+
+      if (report.length) {
+        await this.cache.setReport(job.data.id, report);
+      }
+
+      return report;
     } catch (err) {
       console.error('jobId: ', job.id);
       console.error((<Error>err).stack);
