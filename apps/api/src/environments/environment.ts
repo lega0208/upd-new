8b67b0664de278051e460c1651c7c5e530df46f5
lega0208 <marclegault@Live.com>
2024-04-11 13:36:38 +0000

    Update database config to accept env variables

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  dbHost: process.env.DB_HOST,
   redisHost: process.env.REDIS_HOST || 'localhost',
   redisUsername: 'default',
   redisPassword: process.env.REDIS_PASSWORD,
