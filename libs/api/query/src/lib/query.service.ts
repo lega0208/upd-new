6ea7c72b1944391890469cc9c1b80c3fec991835
amonsour <pureleb@gmail.com>
2024-07-18 14:33:38 +0000

    [Custom Report : Query service] Updated the pages to grab and sort the visits

--- libs/api/query/src/lib/query.service.ts
+++ libs/api/query/src/lib/query.service.ts
@@ -1,7 +1,8 @@
-import { DbService } from '@dua-upd/db';
+import { DbService, Page } from '@dua-upd/db';
 import { Injectable } from '@nestjs/common';
 import { Model, Types, isValidObjectId } from 'mongoose';
 import type { DbQuery } from '@dua-upd/types-common';
+import dayjs from 'dayjs';
 
 @Injectable()
 export class QueryService {
@@ -9,6 +10,11 @@ export class QueryService {
 
   async getData(serializedQueries: { [key: string]: string }) {
     const results: { [key: string]: unknown } = {};
+
+    const queryDateRange = {
+      start: dayjs().subtract(1, 'week').startOf('week').toDate(),
+      end: dayjs().subtract(1, 'week').endOf('week').toDate(),
+    };
 
     for (const key in serializedQueries) {
       const query: DbQuery[keyof DbQuery] = JSON.parse(
@@ -26,11 +32,17 @@ export class QueryService {
       // large queries on this collection can cripple the db, so we'll limit the results like this for now
       const limit = collection === 'pageMetrics' ? 1000 : undefined;
 
-      results[key] = await collectionModel
-        .find(filter, project, { limit })
-        .sort(sort)
-        .lean()
-        .exec();
+      results[key] =
+        collection === 'pages'
+          ? await this.db.views.pageVisits.getVisitsWithPageData(
+              queryDateRange,
+              collectionModel as Model<Page>,
+            )
+          : await collectionModel
+              .find(filter, project, { limit })
+              .sort(sort)
+              .lean()
+              .exec();
     }
 
     return results;
