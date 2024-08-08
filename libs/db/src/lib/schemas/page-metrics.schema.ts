4360978b551c6a25f6668c44b2a0da4a84c25867
lega0208 <20374078+lega0208@users.noreply.github.com>
2024-08-08 18:03:38 +0000

    Create and share common metrics schema

--- libs/db/src/lib/schemas/page-metrics.schema.ts
+++ libs/db/src/lib/schemas/page-metrics.schema.ts
@@ -1,18 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { type Document, Types, type FilterQuery } from 'mongoose';
 import type {
-  GscSearchTermMetrics,
-  AccumulatorOperator,
   AASearchTermMetrics,
-  IPageMetrics,
+  AccumulatorOperator,
   ActivityMapMetrics,
-  ITask,
-  IProject,
-  IUxTest,
+  DateRange,
   IPage,
+  IPageMetrics,
+  IProject,
+  ITask,
+  IUxTest,
 } from '@dua-upd/types-common';
 import {
-  type DateRange,
   ModelWithStatics,
   arrayToDictionary,
   dateRangeSplit,
@@ -20,200 +19,17 @@ import {
 } from '@dua-upd/utils-common';
 import type { Page } from './page.schema';
 import type { PageMetricsTS } from './page-metrics-ts.schema';
+import { MetricsCommon } from './metrics-common.schema';
 
 export type PageMetricsDocument = PageMetrics & Document;
 
 @Schema({ collection: 'pages_metrics' })
-export class PageMetrics implements IPageMetrics {
-  @Prop({ type: Types.ObjectId, required: true })
-  _id: Types.ObjectId = new Types.ObjectId();
-
-  @Prop({ required: true, type: String, index: true })
-  url = '';
-
+export class PageMetrics extends MetricsCommon implements IPageMetrics {
   @Prop({ required: true, type: Date, index: true })
   date = new Date(0);
 
-  @Prop({ type: Number })
-  dyf_submit = 0;
-
-  @Prop({ type: Number })
-  dyf_yes = 0;
-
-  @Prop({ type: Number })
-  dyf_no = 0;
-
-  @Prop({ type: Number })
-  views = 0;
-
-  @Prop({ type: Number })
-  visits = 0;
-
-  @Prop({ type: Number })
-  visitors = 0;
-
-  @Prop({ type: Number })
-  average_time_spent = 0;
-
-  @Prop({ type: Number })
-  bouncerate = 0;
-
-  @Prop({ type: Number })
-  rap_initiated = 0;
-
-  @Prop({ type: Number })
-  rap_completed = 0;
-
-  @Prop({ type: Number })
-  nav_menu_initiated = 0;
-
-  @Prop({ type: Number })
-  rap_cant_find = 0;
-
-  @Prop({ type: Number })
-  rap_login_error = 0;
-
-  @Prop({ type: Number })
-  rap_other = 0;
-
-  @Prop({ type: Number })
-  rap_sin = 0;
-
-  @Prop({ type: Number })
-  rap_info_missing = 0;
-
-  @Prop({ type: Number })
-  rap_securekey = 0;
-
-  @Prop({ type: Number })
-  rap_other_login = 0;
-
-  @Prop({ type: Number })
-  rap_gc_key = 0;
-
-  @Prop({ type: Number })
-  rap_info_wrong = 0;
-
-  @Prop({ type: Number })
-  rap_spelling = 0;
-
-  @Prop({ type: Number })
-  rap_access_code = 0;
-
-  @Prop({ type: Number })
-  rap_link_not_working = 0;
-
-  @Prop({ type: Number })
-  rap_404 = 0;
-
-  @Prop({ type: Number })
-  rap_blank_form = 0;
-
-  @Prop({ type: Number })
-  fwylf_cant_find_info = 0;
-
-  @Prop({ type: Number })
-  fwylf_other = 0;
-
-  @Prop({ type: Number })
-  fwylf_hard_to_understand = 0;
-
-  @Prop({ type: Number })
-  fwylf_error = 0;
-
-  @Prop({ type: Number })
-  visits_geo_ab = 0;
-
-  @Prop({ type: Number })
-  visits_geo_bc = 0;
-
-  @Prop({ type: Number })
-  visits_geo_mb = 0;
-
-  @Prop({ type: Number })
-  visits_geo_nb = 0;
-
-  @Prop({ type: Number })
-  visits_geo_nl = 0;
-
-  @Prop({ type: Number })
-  visits_geo_ns = 0;
-
-  @Prop({ type: Number })
-  visits_geo_nt = 0;
-
-  @Prop({ type: Number })
-  visits_geo_nu = 0;
-
-  @Prop({ type: Number })
-  visits_geo_on = 0;
-
-  @Prop({ type: Number })
-  visits_geo_outside_canada = 0;
-
-  @Prop({ type: Number })
-  visits_geo_pe = 0;
-
-  @Prop({ type: Number })
-  visits_geo_qc = 0;
-
-  @Prop({ type: Number })
-  visits_geo_sk = 0;
-
-  @Prop({ type: Number })
-  visits_geo_us = 0;
-
-  @Prop({ type: Number })
-  visits_geo_yt = 0;
-
-  @Prop({ type: Number })
-  visits_referrer_other = 0;
-
-  @Prop({ type: Number })
-  visits_referrer_searchengine = 0;
-
-  @Prop({ type: Number })
-  visits_referrer_social = 0;
-
-  @Prop({ type: Number })
-  visits_referrer_typed_bookmarked = 0;
-
-  @Prop({ type: Number })
-  visits_device_other = 0;
-
-  @Prop({ type: Number })
-  visits_device_desktop = 0;
-
-  @Prop({ type: Number })
-  visits_device_mobile = 0;
-
-  @Prop({ type: Number })
-  visits_device_tablet = 0;
-
-  @Prop({ type: Number })
-  gsc_total_clicks = 0;
-
-  @Prop({ type: Number })
-  gsc_total_ctr = 0;
-
-  @Prop({ type: Number })
-  gsc_total_impressions = 0;
-
-  @Prop({ type: Number })
-  gsc_total_position = 0;
-
-  @Prop({
-    type: [
-      {
-        clicks: Number,
-        ctr: Number,
-        impressions: Number,
-        position: Number,
-        term: String,
-      },
-    ],
-  })
-  gsc_searchterms: GscSearchTermMetrics[] = [];
+  @Prop({ required: true, type: String, index: true })
+  url = '';
 
   @Prop({ type: [{ term: String, clicks: Number, position: Number }] })
   aa_searchterms?: AASearchTermMetrics[];
@@ -248,6 +64,16 @@ PageMetricsSchema.index(
   { date: 1, page: 1 },
   { background: true, partialFilterExpression: { page: { $exists: true } } },
 );
+
+PageMetricsSchema.index(
+  { date: 1, tasks: 1 },
+  {
+    name: 'date_1_tasks_exists',
+    background: true,
+    partialFilterExpression: { 'tasks.0': { $exists: true } },
+  },
+);
+
 PageMetricsSchema.index(
   { date: 1, projects: 1 },
   {
