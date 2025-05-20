00aa516fc0381df83998d6e958b98b36acbc5230
lega0208 <20374078+lega0208@users.noreply.github.com>
2025-05-20 10:05:08 -0400

    feat: Add DocumentDB config to DB module

--- libs/db/src/lib/db.module.ts
+++ libs/db/src/lib/db.module.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import zstd from '@mongodb-js/zstd'; // need to import this for it to be included in the build output
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, type MongooseModuleOptions } from '@nestjs/mongoose';
 import {
   AAItemId,
   AAItemIdSchema,
@@ -112,26 +112,27 @@ export class DbModule {
     console.log(connectionString);
 
     // Settings for DocumentDB
-    // const config: MongooseModuleOptions = production
-    //   ? {
-    //       ssl: true,
-    //       tlsCAFile: process.env.MONGO_TLS_CA_FILE,
-    //       auth: {
-    //         username: process.env.MONGO_USERNAME,
-    //         password: process.env.MONGO_PASSWORD,
-    //       },
-    //       replicaSet: 'rs0',
-    //       readPreference: 'secondaryPreferred',
-    //       retryWrites: false,
-    //     }
-    //   : {};
+    const config: MongooseModuleOptions =
+      production && process.env.MONGO_USERNAME
+        ? {
+            ssl: true,
+            tlsCAFile: process.env.MONGO_TLS_CA_FILE,
+            auth: {
+              username: process.env.MONGO_USERNAME,
+              password: process.env.MONGO_PASSWORD,
+            },
+            replicaSet: 'rs0',
+            readPreference: 'secondaryPreferred',
+            retryWrites: false,
+          }
+        : {};
 
     return MongooseModule.forRoot(connectionString, {
       connectionName: 'defaultConnection',
       dbName,
       compressors: ['zstd', 'snappy', 'zlib'],
       retryWrites: true,
-      // ...config,
+      ...config,
     });
   }
 }
