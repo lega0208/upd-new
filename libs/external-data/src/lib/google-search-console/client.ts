6773a613cb96d7c92cdf34321a7a9de2b0fd0018
lega0208 <20374078+lega0208@users.noreply.github.com>
2025-08-18 10:54:25 -0400

    fix: Ensure proper private key format for GCS client & small type fix for AA client

--- libs/external-data/src/lib/google-search-console/client.ts
+++ libs/external-data/src/lib/google-search-console/client.ts
@@ -1,5 +1,17 @@
 import { auth, searchconsole_v1 } from '@googleapis/searchconsole';
 
+function formatPrivateKey(key: string): string {
+  return key
+    .replace(
+      /-----BEGIN PRIVATE KEY-----(?!\n)(.)/,
+      '-----BEGIN PRIVATE KEY-----\n$1',
+    )
+    .replace(
+      /(?!\n)(.)-----END PRIVATE KEY-----/,
+      '$1\n-----END PRIVATE KEY-----\n',
+    );
+}
+
 export function getGscClient() {
   return new searchconsole_v1.Searchconsole({
     auth: new auth.GoogleAuth({
@@ -8,7 +20,7 @@ export function getGscClient() {
         'https://www.googleapis.com/auth/webmasters.readonly',
       ],
       credentials: process.env.GSC_KEY && {
-        private_key: process.env.GSC_KEY,
+        private_key: formatPrivateKey(process.env.GSC_KEY),
         client_email: process.env.GSC_EMAIL,
       },
     }),
