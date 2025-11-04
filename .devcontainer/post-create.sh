dcf7f6dcc60a199876b916a7af49381dc186ffd1
amonsour <pureleb@gmail.com>
2025-11-04 11:21:41 -0500

    feat: aws-sso

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,6 +4,8 @@ set -e
 bun install --ignore-scripts --omit=peer --omit=optional && \
   chmod +x .devcontainer/get-duckdb-node-binding.sh && \
   .devcontainer/get-duckdb-node-binding.sh && \
+  chmod +x .devcontainer/aws-sso-init.sh && \
+  .devcontainer/aws-sso-init.sh && \
   npm run mongo-parquet:sync && \
   npm run mongo-parquet:seed-mongo && \
   npm run mongo-parquet:recalc-views-sample
