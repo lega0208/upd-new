9824006eaefac67ab0efb80b0ff97237e00a8736
kolekrstev <65304466+kolekrstev@users.noreply.github.com>
2025-11-07 18:32:38 -0500

    Updates v4.0.0 - Top Tasks updates

--- libs/db/src/lib/views/tasks.view.ts
+++ libs/db/src/lib/views/tasks.view.ts
@@ -982,6 +982,7 @@ export class TasksViewService extends DbViewNew<
 
     const comparisonProps = [
       'calls',
+      'visits',
       'dyf_no',
       'calls_per_100_visits',
       'dyf_no_per_1000_visits',
@@ -996,7 +997,7 @@ export class TasksViewService extends DbViewNew<
     );
 
     return getArraySelectedAbsoluteChange(
-      ['calls_per_100_visits', 'dyf_no_per_1000_visits'],
+      ['calls_per_100_visits', 'dyf_no_per_1000_visits', 'visits', 'calls'],
       '_id',
       dataWithPercentChange,
       // Need to cast it to the same type to get the correct type for the return value
