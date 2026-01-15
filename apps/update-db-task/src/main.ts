7e3d7c35d81dcc02d94ddefbb1bc6da10233c5e9
lega0208 <20374078+lega0208@users.noreply.github.com>
2026-01-15 09:37:11 -0500

    fix: Add 5 minute timeout to prevent hanging process in update-db-task

--- apps/update-db-task/src/main.ts
+++ apps/update-db-task/src/main.ts
@@ -1,14 +1,33 @@
-import { Logger } from '@nestjs/common';
+import { ConsoleLogger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { UpdateTaskService } from './app/app.service';
+import { wait, minutes } from '@dua-upd/utils-common';
 
-const app = await NestFactory.createApplicationContext(AppModule);
+class NoColourLogger extends ConsoleLogger {
+  colorize(message: string, logLevel: string): string {
+    return message;
+  }
 
-Logger.log(
-  `🚀 DB Update task starting`,
-);
+  formatContext(context: string): string {
+    return `[${context}] `;
+  }
+}
 
-await app.select(AppModule).get(UpdateTaskService).updateDatabase();
+async function bootstrap() {
+  const logger = new NoColourLogger('update-db-task');
+  const app = await NestFactory.createApplicationContext(AppModule, { logger });
 
-await app.enableShutdownHooks().close();
+  logger.log(`🚀 DB Update task starting`);
+
+  await app.select(AppModule).get(UpdateTaskService).updateDatabase();
+
+  return Promise.race([
+    wait(minutes(5)), // end the process after 5 minutes
+    app.close(),
+  ]);
+}
+
+bootstrap().then(() => {
+  process.exit(0);
+});
